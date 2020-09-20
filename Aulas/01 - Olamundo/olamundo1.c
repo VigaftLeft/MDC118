301aa2b1fcf868cb01bfb6ace428c9c598e1eac1
VigaftLeft <brunobragancadosreis@gmail.com>
2020-09-20 12:30:53 -0300

    Ola mundo, algumas mudancas

---
+++ Aulas/01 - Olamundo/olamundo1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("olamundo\n");
+
+    /* por algum motivo de que nao sei o return 0; nao e necessario para o codigo */
+}
---
+++ Aulas/01 - Olamundo/olamundo3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char a = 'o';
+    char b = 'l';
+    char c = 'a';
+    char d = ' ';
+    char e = 'm';
+    char f = 'u';
+    char g = 'n';
+    char h = 'd';
+    char i = 'o';
+    char j = '!';
+
+    printf("%c%c%c%c%c%c%c%c%c%c\n", a , b , c , d , e , f , g , h , i , j);
+
+    return 0;
+}
