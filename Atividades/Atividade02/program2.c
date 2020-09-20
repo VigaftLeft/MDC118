986c31e9a03af53e7f3c592d030e03bba0347d9b
VigaftLeft <brunobragancadosreis@gmail.com>
2020-09-20 17:03:40 -0300

    altercao de estetica

--- Atividades/Atividade02/program2.c
+++ Atividades/Atividade02/program2.c
@@ -9,19 +9,19 @@ int main(void)
         if (preco <= 100)
         {
             float desconto = 0.99;
-            float desconto_real = -desconto * 100 + 100;
+            float desconto_real = - desconto * 100 + 100;
             printf("seu preco com desconto e: %.2f, seu desconto foi de %.0f por cento.\n", preco * desconto, desconto_real);
         }
         else if (preco >= 100.01 && preco <= 500)
         {
             float desconto = 0.95;
-            float desconto_real = -desconto * 100 + 100;
+            float desconto_real = - desconto * 100 + 100;
             printf("seu preco com desconto e: %.2f, seu desconto foi de %.0f por cento.\n", preco * desconto, desconto_real);
         }
         else
         {
             float desconto = 0.90;
-            float desconto_real = -desconto * 100 + 100;
+            float desconto_real = - desconto * 100 + 100;
             printf("seu preco com desconto e: %.2f, seu desconto foi de %.0f por cento.\n", preco * desconto, desconto_real);
         }
     }
