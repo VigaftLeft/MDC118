59f5c1201cb898de0555f1a98587c2d33ff3eec9
VigaftLeft <brunobragancadosreis@gmail.com>
2020-10-02 12:18:54 -0300

    incremento do scanf

--- Atividades/Atividade02/program3.c
+++ Atividades/Atividade02/program3.c
@@ -2,28 +2,37 @@
 
 int main(void)
 {
-    int nota; //basta igualar a nota a um numero.
+    short int nota; //basta igualar a nota a um numero.
+    printf("Qual foi sua nota (1 a 5): ");
+    scanf("%hi", &nota);
 
-    switch (nota)
+    if (nota < 1 || nota > 5)
     {
-    case 1:
-        printf("Ruim\n");
-        break;
-    case 2:
-        printf("Insuficiente\n");
-        break;
-    case 3:
-        printf("Suficiente\n");
-        break;
-    case 4:
-        printf("Bom\n");
-        break;
-    case 5:
-        printf("Ótimo\n");
-        break;
-    default:
-        printf("os valores so podem ser de 1 a 5\n");
-        break;
+        printf("Resposta Inválida\n");
+    }
+    else
+    {
+        switch (nota)
+        {
+        case 1:
+            printf("Ruim\n");
+            break;
+        case 2:
+            printf("Insuficiente\n");
+            break;
+        case 3:
+            printf("Suficiente\n");
+            break;
+        case 4:
+            printf("Bom\n");
+            break;
+        case 5:
+            printf("Ótimo\n");
+            break;
+        default:
+            printf("os valores so podem ser de 1 a 5\n");
+            break;
+        }
     }
 
     return 0;
