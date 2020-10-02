af17ea38de19c329d8dbb6b3a43f4b52610eb753
VigaftLeft <brunobragancadosreis@gmail.com>
2020-10-02 11:49:18 -0300

    correcao do codigo

--- Atividades/Atividade01/program2.c
+++ Atividades/Atividade01/program2.c
@@ -2,11 +2,20 @@
 
 int main(void)
 {
-    int segundo_em_hora; //basta colocar qualuqer valor em segundo em hroa
-    int hora_em_segundo; //basta colocar qualquer valor em hora em segundo
+    float x;
+    printf("Qual é o valor de x de segundos: ");
+    scanf("%f", &x);
 
-    printf("segundos em horas: %i\n", segundo_em_hora * 1/3600);
-    printf("horas para segundos: %i\n", hora_em_segundo * 60);
-    
+    if (x < 0)
+    {
+        printf("Resposta inválida\n");
+    }
+    else
+    {
+        printf("%.0f segundos em horas: %.3f \n", x, x / 3600);
+        printf("%.0f em minutos: %.3f\n", x, x / 60);
+        printf("%.0f em segundos: %.0f\n", x, x);
+    }
+
     return 0;
 }
