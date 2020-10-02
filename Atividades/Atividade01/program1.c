9db4ac08e45b4ad2618c5f87765e33b3f2415444
VigaftLeft <brunobragancadosreis@gmail.com>
2020-10-02 11:32:04 -0300

    incremento do scanf e if else

--- Atividades/Atividade01/program1.c
+++ Atividades/Atividade01/program1.c
@@ -5,11 +5,21 @@ const float pi = 3.1416;
 int main(void)
 {
     float perimetro; // basta colocar qualuqer valor em perimetro.
-    float raio; //basta colocar qualquer valor em rario.
+    float raio;      //basta colocar qualquer valor em rario.
 
-    perimetro = 2 * pi * raio;
+    printf("Qual é o raio: ");
+    scanf("%f", &raio);
 
-    printf("Perimetro da circunferencia: %f\n", perimetro);
+    if (raio < 0)
+    {
+        printf("Resposta Inválida\n");
+    }
+    else
+    {
+        perimetro = 2 * pi * raio;
+
+        printf("Perimetro da circunferencia: %.2f\n", perimetro);
+    }
 
     return 0;
 }
