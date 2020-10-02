036656bc348adc182615db1716aa709fe01d7f98
VigaftLeft <brunobragancadosreis@gmail.com>
2020-10-02 11:59:53 -0300

    incremento do scanf

--- Atividades/Atividade01/program3.c
+++ Atividades/Atividade01/program3.c
@@ -2,9 +2,19 @@
 
 int main(void)
 {
-    long double bytes_por_giga = 1.0; //basta colocar qualquer valor
+    long double giga_em_byt; //basta colocar qualquer valor
+    printf("Valor de Gigabytes: ");
+    scanf("%LF", &giga_em_byt);
+    
+    if (giga_em_byt < 0)
+    {
+        printf("Valor Inválido");
+    }
+    else
+    {
+        printf("%.0LF em bytes = %.3LF\n", giga_em_byt, giga_em_byt * 1024 * 1024 * 1024);
 
-    printf("Quantos bytes tem %.2LF gigas: %LF \n", bytes_por_giga, bytes_por_giga * 1024 * 1024 * 1024);
-
+    }
+    
     return 0;
 }
