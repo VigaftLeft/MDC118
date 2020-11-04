448729c9e03a41837166499fa89606cd061c1691
VigaftLeft <brunobragancadosreis@gmail.com>
2020-11-04 09:49:21 -0300

    .

--- Atividades/Atividade05/program3.c
+++ Atividades/Atividade05/program3.c
@@ -1,27 +1,26 @@
-
 #include <stdio.h>
 
+const int TAMANHO = 80;
 int main()
 {   
-    int TAMANHO = 80;
     int array[TAMANHO];
-    printf("digite o numero: ");
 
     for (int i = 0; i < TAMANHO; i++)
     {
+        printf("digite o numero: ");
         scanf("%i", &array[i]);
-        if (array[i] == 's')
-        {
-            i = TAMANHO;
-            for (int j = i -1; j > -1; j--)
-            {
-                printf("%i", array[j]);
-            }
-        }
-        printf("%i", i);
     }
-
-    
+    for (int i = TAMANHO -1; i > -1; i--) //menos um pois o array so conta a partir do 0 logo o fim é menos 1
+    {
+        if (i == 0)
+        {
+            printf("%i.", array[i]);
+        }
+        else
+        {
+            printf("%i, ", array[i]);
+        }
+    }
     printf("\n");
 
     return 0;
